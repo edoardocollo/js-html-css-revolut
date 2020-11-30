144bc8ae6b4896c44611574dfa8442ab9626745e
edoardocollo <edoardo.collo@gmail.com>
2020-11-30 16:58:33 +0100

    rebuild

---
+++ main.js
@@ -0,0 +1,15 @@
+var linkLista = $('li');
+linkLista.hover(function(){
+  jQuery('.palette', this).toggle();
+});
+
+
+
+
+
+// var wrapper = $('.wrapper');
+// // var ul = $('ul');
+// var paletteWrapper = $('.palette_wrapper');
+// wrapper.hover(function(){
+//   jQuery('.palette_wrapper', this).toggle();
+// });
